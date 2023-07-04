ec64e5b85136e6898858c608de348fd98855f0f9
Emill <lemito66@gmail.com>
2023-07-04 17:14:49 -0500

    modelo

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    dni = models.IntegerField()
+    telefono = models.IntegerField()
+    email = models.EmailField()
+    direccion = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    fecha_creacion = models.DateField(auto_now_add=True)
+    numero_atencion = models.IntegerField()
+    # como hacer primary key
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellido
--- pacientes_crud_api/settings.py
+++ pacientes_crud_api/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Cors authorization
+CORS_ALLOWED_ORIGINS = [
+    # "https://example.com",
+    # "https://sub.example.com",
+    # "http://localhost:8080",
+    # "http://127.0.0.1:9000",
+]
