20e38aa9b184e4435de2e5af03c17740728df52f
Emill <lemito66@gmail.com>
2023-07-05 14:45:40 -0500

    creando el modelo

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,19 +1,17 @@
 from django.db import models
-
 # Create your models here.
 class Paciente(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=75 )
+    apellido = models.CharField(max_length=75)
     edad = models.IntegerField()
-    dni = models.IntegerField()
+    cedula = models.IntegerField()
     telefono = models.IntegerField()
     email = models.EmailField()
     direccion = models.CharField(max_length=50)
     fecha_nacimiento = models.DateField()
     fecha_creacion = models.DateField(auto_now_add=True)
-    numero_atencion = models.IntegerField()
-    # como hacer primary key
-    id = models.AutoField(primary_key=True)
+    peso = models.IntegerField()
+    altura = models.IntegerField()
 
     def __str__(self):
-        return self.nombre + ' ' + self.apellido
+        return f'{self.nombre} {self.apellido}'
