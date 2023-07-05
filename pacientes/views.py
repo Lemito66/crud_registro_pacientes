ae809bcfa7147124862d147db0de8eda97e869d3
Emill <lemito66@gmail.com>
2023-07-05 16:10:59 -0500

    vista Pacientes

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializer import PacienteSerializer
+from .models import Paciente
 # Create your views here.
+class PacienteView(viewsets.ModelViewSet):
+    serializer_class = PacienteSerializer
+    queryset = Paciente.objects.all()
