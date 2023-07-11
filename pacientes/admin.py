57e421046332ca764b6d995fae52e3689ec0b4fd
Emill <lemito66@gmail.com>
2023-07-11 15:34:03 -0500

    añadiendo los modelos

--- pacientes/admin.py
+++ pacientes/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Paciente
+from .models import Paciente, Usuario, Direccion, Ocupacion, SeguroSalud, DatosEmergencia
 
 # Register your models here.
 admin.site.register(Paciente)
+admin.site.register(Usuario)
+admin.site.register(Direccion)
+admin.site.register(Ocupacion)
+admin.site.register(SeguroSalud)
+admin.site.register(DatosEmergencia)
